3397843ee0eb36670d3347935d3ae33864754e4d
wilson <whuanca06@gmail.com>
2020-03-28 01:37:34 -0400

    init socket config

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+let server = http.createServer(app);
+
+const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+let io = socketIO(server);
+
+io.on('connection', (client) => {
+    console.log('Usuario conectado');
+
+    client.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+    //escuchar el cliente
+    client.on('enviarMensaje', (message) => {
+        console.log(message)
+    });
+});
+
+// IO = esta es la comunicacion del backEnd
+server.listen(port, (err) => {
+
+    if (err) throw new Error(err);
+
+    console.log(`Servidor corriendo en puerto ${port}`);
+
+});
